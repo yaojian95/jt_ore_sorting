65246a4f79b0eef1a3d96cd5813f0718ff31f2b8
yaojian95 <1427456796@qq.com>
2025-04-22 13:55:26 +0800

    push before merge

--- server_scripts/run.py
+++ server_scripts/run.py
@@ -24,67 +24,14 @@ from classifiers.dual_all_parallel import ParallelClassifier
 from classifiers.demo import Demo
 
 
-path_0219 = os.path.join(data_path, '20250219/160kV_4mA_forward.png')
-excel_0219 = os.path.join(data_path, '化验结果/20250219化验结果.xlsx')
+with open('input_0219_0224_0225.pkl', 'rb') as f:
+    input_all = pickle.load(f)
+pixels = input_all[0]
+data = input_all[1]
 
-excel_0224 = os.path.join(data_path, '化验结果/20250224化验结果.xlsx')
-path_0224 = os.path.join(data_path, '20250224/160kV_4mA_forward.png')
 
-excel_0225 = os.path.join(data_path, '化验结果/20250225化验结果.xlsx')
-path_0225_p1 = os.path.join(data_path, '20250225/160kV_4mA_1_105_forward.png')
-path_0225_p2 = os.path.join(data_path, '20250225/160kV_4mA_106_200_forward.png')
-
-z = path2pixel(path_0219, [[105, 1000, 400, 1200], [1950, 2844, 350, 1150]], max_len = [6, 9], length = [102, 35], 
-           s_i = [64, 1], th_val = 105, save_rock_image = False, direction = 'ublr', save_rock_pixels=False)
-
-consts = [5, 20]; th_val = 110
-
-y = path2pixel(path_0224, [[105, 1000, 400, 1200], [1850,-100, 350,1150]], max_len = [6, 9], length=[102, 35], 
-               s_i = [64, 1], th_val = 110, save_rock_image = False, direction = 'ublr', save_rock_pixels=False)
-
-x1 = path2pixel(path_0225_p1, [[50, 1000, 350, 1150], [1850, -60, 350,1150]], max_len = [6, 9], length=[102, 35], 
-               s_i = [64, 1], th_val = 115, save_rock_image = False, direction = 'ublr', save_rock_pixels=False)
-
-x1[2][0].insert(55, np.array((0, 1)))
-x1[2][1].insert(55, np.array((0, 1)))
-
-x2 = path2pixel(path_0225_p2, [[30, 1000, 350, 1150], [1850, -60, 350,1150]], max_len = [7, 7], length=[35, 35], 
-               s_i = [50, 1], th_val = 115, save_rock_image = False, direction = 'ublr', save_rock_pixels=False)
-
-x2[2][0].insert(19, np.array((0, 1)))
-x2[2][1].insert(19, np.array((0, 1)))
-
-x2[2][0].insert(61, np.array((0, 1)))
-x2[2][1].insert(61, np.array((0, 1)))
-
-x  = [x1[2][0] + x2[2][0], x1[2][1] + x2[2][1]]
-
-input_0224 = load_data(y[2], excel_0224)
-input_0219 = load_data(z[2], excel_0219)
-input_0225 = load_data(x, excel_0225)
-
-input_both = [[],]
-# pixels
-input_both[0].append(input_0224[0][0].copy(deep = True))
-input_both[0][0].index += 99
-
-input_both[0].append(input_0224[0][1].copy(deep = True))
-input_both[0][1].index += 99
-# truth
-input_0219[1]['source'] = '0219'
-input_0224[1]['source'] = '0224'
-input_both.append(input_0224[1].copy(deep = True))
-input_both[1].index += 99
-
-input_both[0][0] = pd.concat([input_0219[0][0], input_both[0][0]], axis = 0)
-input_both[0][1] = pd.concat([input_0219[0][1], input_both[0][1]], axis = 0)
-
-input_both[1]= pd.concat([input_0219[1], input_both[1]], axis = 0)
-
-input_both[1]['Zn_Pb_grade'] = input_both[1]['Zn_grade'] + input_both[1]['Pb_grade']
-
-# print('Success')
-# exit()
+index_0219 = data.loc[data['source'] == 'source_0'].index.values
+input_0219 = [[pixels[0][index_0219], pixels[1][index_0219]], data.loc[index_0219]]
 
 def tune(input_list, input_name = ['0219', '0224', 'both','0225'], step_A = 5, include_Fe = False):
     
@@ -138,7 +85,7 @@ def tune(input_list, input_name = ['0219', '0224', 'both','0225'], step_A = 5, i
 
 res_all = tune([input_0219, input_0224, input_both, input_0225], input_name = ['0219', '0224', 'both','0225'], step_A = 1)
 
-with open('20250414_results_inlucde_0225_v2.pkl', 'wb') as f:
+with open('20250414_results_inlucde_0225_v3.pkl', 'wb') as f:
     pickle.dump(res_all, f) 
 
 # with open ('results.pkl', 'rb') as f:
